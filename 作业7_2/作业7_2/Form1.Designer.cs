fe751d2051b99023913d1d7314b7b64655d7e1cf
joe11111-sudo <joejywong@gmail.com>
2022-04-16 00:55:47 +0800

    7_2

---
+++ 作业7_2/作业7_2/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace 作业7_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(8, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入您的身份证号：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("隶书", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 124);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(705, 60);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(262, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 78);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "验证";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(74, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 33);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "身份证号验证器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
+
---
+++ 作业7_2/作业7_2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 作业7_2
+{
+    public partial class Form1 : Form
+    {
+        int[] weight;
+        string errorMsg;
+        public Form1()
+        {
+            InitializeComponent();
+            weight = new int[] { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };
+            errorMsg = "错误";
+        }
+        private bool check(string str)
+        {
+            int sum = 0;
+            for (int i = 0; i < 17; i++)
+            {
+                sum += (str[i] - '0') * weight[i];
+            }
+            label2.Text = sum.ToString();
+            sum = (12 - sum % 11) % 11;
+            label2.Text += "    " + sum.ToString();
+            if (sum == 10) return str[17] == 'x' || str[17] == 'X';
+            return (sum == str[17] - '0');
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string regex = @"[1-9]{2}\d{4}(19|20)\d{2}[0|1][0-9][0-3][0-9]\d{3}[\d|x|X]\s*";
+            string input = textBox1.Text;
+            System.Text.RegularExpressions.Match match = System.Text.RegularExpressions.Regex.Match(input, regex);
+            if (!match.Success)
+            {
+                MessageBox.Show("身份证号格式不合法", "ERROR! ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (!check(input))
+            {
+                MessageBox.Show("身份证号验证错误", "ERROR! ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                label2.Text = "身份证号合法正确。";
+            }
+        }
+    }
+}
